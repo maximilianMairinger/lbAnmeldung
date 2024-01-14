8040e8d90979541397d3efd0e70ba4b00132c7d0
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-01-14 20:00:09 +0100

    ctp init app

---
+++ app/res/lang/de.ts
@@ -0,0 +1,14 @@
+export default {
+  appName: {
+    short: "lbAnmeldung",
+    long: "Lb anmeldung"
+  },
+  links: {
+    contact: "Kontakt",
+    imprint: "Impressum",
+    privacy: "Datenschutz",
+    terms: "AGB"
+  },
+  confirm: "Bestätigen",
+  copiedFeedback: "Kopiert!",
+}
---
+++ app/res/lang/en.ts
@@ -0,0 +1,14 @@
+export default {
+  appName: {
+    short: "lbAnmeldung",
+    long: "Lb anmeldung"
+  },
+  links: {
+    contact: "Contact",
+    imprint: "Imprint",
+    privacy: "Privacy",
+    terms: "Terms"
+  },
+  confirm: "Confirm",
+  copiedFeedback: "Copied!",
+}
