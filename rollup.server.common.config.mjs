ee6986fe9be8ce527bb3ed93e9752814828044c5
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-01-15 16:07:27 +0100

    core working

--- rollup.server.common.config.mjs
+++ rollup.server.common.config.mjs
@@ -2,20 +2,24 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonJS from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import dotenv from 'rollup-plugin-dotenv'
 
 
 export default {
   input: 'server/src/server.ts',
   output: {
     file: 'server/dist/server.js',
-    format: 'cjs'
+    format: 'cjs',
+    inlineDynamicImports: true,
   },
+  inlineDynamicImports: true,
   plugins: [
     typescript({tsconfig: "./tsconfig.server.json", noEmitOnError: false}), 
     resolve({modulesOnly: true, preferBuiltins: true}),
     commonJS({
       include: 'node_modules/**'
     }),
-    json()
+    json(),
+    dotenv()
   ]
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,206 @@
 import setup from "./setup"
+import PushSaver from "pushsafer-notifications"
+import xrray from "xrray"; xrray()
+import { josmFsReflection } from "josm-adapter"
+import puppeteer, { Locator } from "puppeteer"
+import ms from "millisecond"
+import delay from "tiny-delay"
 
+
+if (process.env.PUSHSAFER_KEY === undefined) throw new Error("No pushsafer key provided")
+if (process.env.TGM_USERNAME === undefined) throw new Error("No tgm username provided")
+if (process.env.TGM_PASSWORD === undefined) throw new Error("No tgm password provided")
+
+
+
+function log(...msg) {
+  console.log(timeNow(), ...msg)
+}
+
+function logError(...msg) {
+  console.error(timeNow(), ...msg)
+}
+
+
+function timeNow() {
+  return new Date().toLocaleString("de-AT", { timeZone: "Europe/Vienna" })
+}
 
 
 setup("lbAnmeldung").then(async ({app, db}) => {
+  const file = await josmFsReflection("./public/res/changes.json", {length: 0})
+
+  const p = new PushSaver({
+    k: process.env.PUSHSAFER_KEY as string,
+    debug: false
+  });
+
+
+  let working = true
+  let lastTxt = await checkWithPup()
+  sendMsgToClients("Started and operational")
+  setInterval(async () => {
+    const newTxt = await checkWithPup()
+    try {
+      if (!working) {
+        sendMsgToClients("Operational")
+      }
+      working = true
+      if (lastTxt !== newTxt) {
+        const len = file.length.get()
+        file({length: len + 1, [len]: {time: timeNow(), txt: newTxt}})
+        sendMsgToClients("Change detected", 1)
+      }
+    }
+    catch(e) {
+      logError(e)
+      if (working) {
+        sendMsgToClients("Not operational")
+      }
+      working = false
+    }
+  }, ms("20min"))
+  
+  
+
+
+
+
+
+
+  function sendMsgToClients(msg: string, priority = 0) {
+    return new Promise((res, rej) => {
+      log(msg)
+      p.send( {m: msg, pr: priority}, ( err, result ) => {
+        res({ok: !err, result, err})
+      });
+    })
+  }
 
   
-  app.post("/echo", (req, res) => {
-    res.send(req.body)
-  })
 })
+
+
+
+
+async function checkWithPup() {
+  log("starting pup")
+
+  const browser = await puppeteer.launch({headless: 'new'});
+  const page = await browser.newPage();
+  const timeout = 5000;
+  page.setDefaultTimeout(timeout);
+
+  {
+    const targetPage = page;
+    await targetPage.setViewport({
+        width: 1010,
+        height: 876
+    })
+  }
+  {
+    const targetPage = page;
+    const promises = [] as any[];
+    const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
+    }
+    startWaitingForEvents();
+    await targetPage.goto('https://lb-anmeldung.tgm.ac.at');
+    await Promise.all(promises);
+  }
+  // {
+  //   const targetPage = page;
+  //   await (Locator.race([
+  //       targetPage.locator('::-p-aria(TGM Benutzername)'),
+  //       targetPage.locator('form > div > div:nth-of-type(1) input'),
+  //       targetPage.locator('::-p-xpath(//*[@id=\\"app\\"]/div[2]/div/div/div/div/form/div/div[1]/div/input)'),
+  //       targetPage.locator(':scope >>> form > div > div:nth-of-type(1) input')
+  //   ]) as any)
+  //       .setTimeout(timeout)
+  //       .click({
+  //         offset: {
+  //           x: 163.953125,
+  //           y: 26.65625,
+  //         },
+  //       });
+  // }
+  {
+    const targetPage = page;
+    await (Locator.race([
+        targetPage.locator('::-p-aria(TGM Benutzername)'),
+        targetPage.locator('form > div > div:nth-of-type(1) input'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"app\\"]/div[2]/div/div/div/div/form/div/div[1]/div/input)'),
+        targetPage.locator(':scope >>> form > div > div:nth-of-type(1) input')
+    ]) as any)
+        .setTimeout(timeout)
+        .fill(process.env.TGM_USERNAME as string);
+  }
+  {
+    const targetPage = page;
+    await targetPage.keyboard.down('Tab');
+  }
+  {
+    const targetPage = page;
+    await targetPage.keyboard.up('Tab');
+  }
+  {
+    const targetPage = page;
+    await (Locator.race([
+        targetPage.locator('::-p-aria(Passwort)'),
+        targetPage.locator('form > div > div:nth-of-type(2) input'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"app\\"]/div[2]/div/div/div/div/form/div/div[2]/div/input)'),
+        targetPage.locator(':scope >>> form > div > div:nth-of-type(2) input')
+    ]) as any)
+        .setTimeout(timeout)
+        .fill(process.env.TGM_PASSWORD as string);
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
+    }
+    await Locator.race([
+        targetPage.locator('::-p-aria(Anmelden)'),
+        targetPage.locator('#app > div.container button'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"app\\"]/div[2]/div/div/div/div/form/div/button)'),
+        targetPage.locator(':scope >>> #app > div.container button'),
+        targetPage.locator('::-p-text(Anmelden)')
+    ])
+        .setTimeout(timeout)
+        .on('action', () => startWaitingForEvents())
+        .click({
+          offset: {
+            x: 89.953125,
+            y: 31.65625,
+          },
+        });
+    await Promise.all(promises);
+  }
+
+  await delay(3000)
+  
+  const str = await page.evaluate(() => {
+    const el = document.querySelector("#app > div.ui.container")
+    if (el === null) return null
+    return el.textContent
+  })
+
+  if (str === null) {
+    throw new Error("Could not find the text")
+  }
+
+  await browser.close();
+
+  log("done pup")
+
+  return str
+}
+
+
+
+
+
+
+
+
